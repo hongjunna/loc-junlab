82dfd0e1ef75766864567e463b25517224662057
hongjunna <jun@junlab.xyz>
2026-01-01 19:22:42 +0900

    Fix: apply .gitignore and remove node_modules from tracking

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,17 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import routeRoutes from './routes/route';
-import * as dotenv from 'dotenv'; // 추가
-
-dotenv.config(); // .env 파일의 내용을 process.env에 로드
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // 몽고DB 연결 (도커 컨테이너 이름이 'mongodb'라고 가정)
-const MONGO_URI =
-  process.env.MONGO_URI || 'mongodb://localhost:27017/bus-project';
+const MONGO_URI = 'mongodb://admin:wnsfoq1!@mongodb.junlab.xyz:45761/';
 
 mongoose
   .connect(MONGO_URI)
@@ -23,7 +19,7 @@ mongoose
 app.get('/', (req, res) => res.send('Bus Tracking Server Running'));
 app.use('/api', routeRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5679;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
